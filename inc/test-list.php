2232e47da288062485d8ea14f1da24c43578b899
Zarkana <jsc940@gmail.com>
2017-01-06 13:26:12 -1000

    add select menu styles for test list page

---
+++ inc/test-list.php
@@ -0,0 +1,145 @@
+
+<h2 class="header center light-blue-text text-lighten-1 test-header">
+    <div class="left-header">
+      Testing Yourself in
+    </div>
+    <div class="input-field right-header">
+      <select>
+        <option value="" disabled selected>What?</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </div>
+</h2>
+
+<div class="row center test-list">
+  <div class="col s6 offset-s3">
+    <ul class="collapsible" data-collapsible="accordion">
+      <li>
+        <div class="collapsible-header">
+          Hello World!
+          <div class="switch">
+            <label>
+              <input checked type="checkbox">
+              <span class="lever"></span>
+            </label>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="collapsible-header expandable">
+          Algorithms - Sorting
+          <div class="switch">
+            <label>
+              <input checked type="checkbox">
+              <span class="lever"></span>
+            </label>
+          </div>
+        </div>
+        <div class="collapsible-body">
+          <ul class="collapsible sub-menu" data-collapsible="accordion">
+            <li>
+              <div class="collapsible-header">
+                Bubble Sort
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header">
+                Merge Sort
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header">
+                Heapsort
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header">
+                Insertion Sort
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+              <div class="collapsible-header">
+                Selection Sort
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="collapsible-header expandable">
+          Algorithms - Search
+          <div class="switch">
+            <label>
+              <input checked type="checkbox">
+              <span class="lever"></span>
+            </label>
+          </div>
+        </div>
+        <div class="collapsible-body">
+          <ul class="collapsible sub-menu" data-collapsible="accordion">
+            <li>
+              <div class="collapsible-header">
+                Linear Search
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header">
+                Binary Search
+                <div class="switch">
+                  <label>
+                    <input checked type="checkbox">
+                    <span class="lever"></span>
+                  </label>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <!--<li>
+        <div class="collapsible-header">Web Development</div>
+        <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+      </li>-->
+    </ul>
+  </div>
+</div>
+<div class="row center">
+  <a href="#" id="download-button" class="btn-large waves-effect waves-light blue">Test</a>
+</div>
+<br><br>
