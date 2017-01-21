08111aaa64a2f336086b60e9ffd79f2e010fe281
Zarkana <jsc940@gmail.com>
2017-01-20 14:20:20 -1000

    add working links between quiz 'index' and quiz 'question'

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,4 +1,7 @@
 class QuizController < ApplicationController
   def index
   end
+
+  def question
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'quiz/index'
-
+  get 'quiz/question', to: 'quiz#question'
   get 'welcome/index'
 
   root 'welcome#index'
+  #get '/quiz', to: 'controller_name#press'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
