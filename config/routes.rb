e3d5ad90b8a5f35d9333d5c29d76317bce90d7cb
Zarkana <jsc940@gmail.com>
2017-01-23 16:13:33 -1000

    add study controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'study/index'
+
   get 'quiz/index'
   get 'quiz/question', to: 'quiz#question'
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'quiz#index'
   #get '/quiz', to: 'controller_name#press'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
